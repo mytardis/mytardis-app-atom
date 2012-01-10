2793fa15a36ea8fbf45fe68f076ffd2414463ac3
Tim Dettrick <t.dettrick@uq.edu.au>
2012-01-10 14:28:02 +1000

    Seperated Persister and Walker TestCases for neatness.

--- tests/test_atom_ingest.py
+++ tests/test_atom_ingest.py
@@ -8,6 +8,81 @@ from flexmock import flexmock, flexmock_teardown
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import BaseHTTPServer, base64, os, inspect, SocketServer, threading, urllib2
 
+class TestWebServer:
+    '''
+    Utility class for running a test web server with a given handler.
+    '''
+
+    class QuietSimpleHTTPRequestHandler(SimpleHTTPRequestHandler):
+        '''
+        Simple subclass that only prints output to STDOUT, not STDERR
+        '''
+        def log_message(self, msg, *args):
+            print msg % args
+
+        def _isAuthorized(self):
+            if self.headers.getheader('Authorization') == None:
+                return False
+            t, creds = self.headers.getheader('Authorization').split(" ")
+            if t != "Basic":
+                return False
+            if base64.b64decode(creds) != "username:password":
+                return False
+            return True
+
+        def do_GET(self):
+            if not self._isAuthorized():
+                self.send_response(401, 'Unauthorized')
+                self.send_header('WWW-Authenticate', 'Basic realm="Test"')
+                self.end_headers()
+                return
+            SimpleHTTPRequestHandler.do_GET(self)
+
+    class ThreadedTCPServer(SocketServer.ThreadingMixIn, \
+                            BaseHTTPServer.HTTPServer):
+        pass
+
+    def __init__(self):
+        self.handler = self.QuietSimpleHTTPRequestHandler
+
+    def start(self):
+        server = self.ThreadedTCPServer(('127.0.0.1', self.getPort()),
+                                        self.handler)
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
+        self.server = server
+        return server.socket.getsockname()
+
+    def getUrl(self):
+        return 'http://%s:%d/' % self.server.socket.getsockname()
+
+    @classmethod
+    def getPort(cls):
+        return 4272
+
+    def stop(self):
+        self.server.shutdown()
+        self.server.socket.close()
+
+
+
+class AbstractAtomServerTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.priorcwd = os.getcwd()
+        os.chdir(os.path.dirname(__file__)+'/atom_test')
+        cls.server = TestWebServer()
+        cls.server.start()
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        os.chdir(cls.priorcwd)
+        cls.server.stop()
+
+
 
 class SchemaTestCase(TestCase):
 
@@ -22,185 +97,9 @@ class SchemaTestCase(TestCase):
             assert schema.subtype != None
             assert schema.id != None
 
-class ProcessorTestCase(TestCase):
-
-    class TestWebServer:
-        '''
-        Utility class for running a test web server with a given handler.
-        '''
-
-        class QuietSimpleHTTPRequestHandler(SimpleHTTPRequestHandler):
-            '''
-            Simple subclass that only prints output to STDOUT, not STDERR
-            '''
-            def log_message(self, msg, *args):
-                print msg % args
-
-            def _isAuthorized(self):
-                if self.headers.getheader('Authorization') == None:
-                    return False
-                t, creds = self.headers.getheader('Authorization').split(" ")
-                if t != "Basic":
-                    return False
-                if base64.b64decode(creds) != "username:password":
-                    return False
-                return True
-
-            def do_GET(self):
-                if not self._isAuthorized():
-                    self.send_response(401, 'Unauthorized')
-                    self.send_header('WWW-Authenticate', 'Basic realm="Test"')
-                    self.end_headers()
-                    return
-                SimpleHTTPRequestHandler.do_GET(self)
-
-        class ThreadedTCPServer(SocketServer.ThreadingMixIn, \
-                                BaseHTTPServer.HTTPServer):
-            pass
-
-        def __init__(self):
-            self.handler = self.QuietSimpleHTTPRequestHandler
-
-        def start(self):
-            server = self.ThreadedTCPServer(('127.0.0.1', self.getPort()),
-                                            self.handler)
-            server_thread = threading.Thread(target=server.serve_forever)
-            server_thread.daemon = True
-            server_thread.start()
-            self.server = server
-            return server.socket.getsockname()
-
-        def getUrl(self):
-            return 'http://%s:%d/' % self.server.socket.getsockname()
-
-        @classmethod
-        def getPort(cls):
-            return 4272
-
-        def stop(self):
-            self.server.shutdown()
 
 
-    @classmethod
-    def setUpClass(cls):
-        cls.priorcwd = os.getcwd()
-        os.chdir(os.path.dirname(__file__)+'/atom_test')
-        cls.server = cls.TestWebServer()
-        cls.server.start()
-        pass
-
-    @classmethod
-    def tearDownClass(cls):
-        os.chdir(cls.priorcwd)
-        cls.server.stop()
-
-    def tearDown(self):
-        flexmock_teardown()
-
-
-    def testWalkerCredentials(self):
-        '''
-        Test that the walker manages credentials.
-        '''
-        address = 'http://localhost:%d/datasets.atom' % \
-                                (self.TestWebServer.getPort())
-        try:
-            urllib2.urlopen(address)
-            ok_(False, 'Should have thrown error')
-        except urllib2.HTTPError:
-            pass
-        opener = urllib2.build_opener((AtomWalker.get_credential_handler()))
-        try:
-            f = opener.open(address)
-            eq_(f.getcode(), 200, 'Should have been: "200 OK"')
-        except urllib2.HTTPError:
-            ok_(False, 'Should not have thrown error')
-        pass
-
-
-    def testWalkerFollowsAtomLinks(self):
-        '''
-        Test that the walker follows links.
-        '''
-        # We build a persister which says all entries are new.
-        persister = flexmock(AtomPersister())
-        persister.should_receive('is_new').with_args(object, object)\
-            .and_return(True).times(4)
-        persister.should_receive('process').with_args(object, object)\
-            .times(4)
-        walker = AtomWalker('http://localhost:%d/datasets.atom' %
-                            (self.TestWebServer.getPort()),
-                            persister)
-        ok_(inspect.ismethod(walker.ingest),"Walker must have an ingest method")
-        walker.ingest()
-
-
-    def testWalkerProcessesEntriesInCorrectOrder(self):
-        '''
-        Test that the walker processes the entries in the revese order that it
-        finds them.
-        '''
-        checked_entries = []
-        processed_entries = []
-        # We build a persister which says all entries are new.
-        persister = flexmock(AtomPersister())
-        # Grab the checked entry and return true
-        persister.should_receive('is_new').with_args(object, object)\
-            .replace_with(lambda feed, entry: checked_entries.append(entry) or True)
-        # Grab the processed entry
-        persister.should_receive('process').with_args(object, object)\
-            .replace_with(lambda feed, entry: processed_entries.append(entry))
-        parser = AtomWalker('http://localhost:%d/datasets.atom' %
-                            (self.TestWebServer.getPort()),
-                            persister)
-        parser.ingest()
-        # We should have checked four entries, chronologically decendent
-        eq_(len(checked_entries), 4)
-        checked_backwards = reduce(self._check_chronological_asc_order,\
-                                     reversed(checked_entries), None)
-        ok_(checked_backwards, "Entries aren't parsed in reverse chronological order")
-        # We should have processed four entries, chronologically ascendent
-        eq_(len(processed_entries), 4)
-        processed_forwards = reduce(self._check_chronological_asc_order,\
-                                     processed_entries, None)
-        ok_(processed_forwards, "Entries aren't parsed in chronological order")
-
-
-    def testWalkerOnlyIngestsNewEntries(self):
-        '''
-        Test that the walker will stop when it gets to an entry that isn't new.
-        '''
-        # We build a persister which says there are three entries
-        # that aren't in the repository.
-        persister = flexmock(AtomPersister())
-        persister.should_receive('is_new').with_args(object, object)\
-            .and_return(True, True, True, False).one_by_one.times(4)
-        persister.should_receive('process').with_args(object, object).times(3)
-        parser = AtomWalker('http://localhost:%d/datasets.atom' %
-                            (self.TestWebServer.getPort()),
-                            persister)
-        parser.ingest()
-        # We build a persister which says there are two entries
-        # that aren't in the repository.
-        persister = flexmock(AtomPersister())
-        persister.should_receive('is_new').with_args(object, object)\
-            .and_return(True, True, False, False).one_by_one.times(4)
-        persister.should_receive('process').with_args(object, object).times(2)
-        parser = AtomWalker('http://localhost:%d/datasets.atom' %
-                            (self.TestWebServer.getPort()),
-                            persister)
-        parser.ingest()
-        # We build a persister which says there is one entry
-        # that isn't in the repository.
-        persister = flexmock(AtomPersister())
-        persister.should_receive('is_new').with_args(object, object)\
-            .and_return(True, False, False, False).one_by_one.times(2)
-        persister.should_receive('process').with_args(object, object).times(1)
-        parser = AtomWalker('http://localhost:%d/datasets.atom' %
-                            (self.TestWebServer.getPort()),
-                            persister)
-        parser.ingest()
-
+class PersisterTestCase(AbstractAtomServerTestCase):
 
     def testPersisterRecognisesNewEntries(self):
         feed, entry = self._getTestEntry()
@@ -250,6 +149,116 @@ class ProcessorTestCase(TestCase):
         return (doc.feed, entry)
 
 
+
+class WalkerTestCase(AbstractAtomServerTestCase):
+
+    def tearDown(self):
+        flexmock_teardown()
+
+
+    def testWalkerCredentials(self):
+        '''
+        Test that the walker manages credentials.
+        '''
+        address = 'http://localhost:%d/datasets.atom' % \
+                                (TestWebServer.getPort())
+        try:
+            urllib2.urlopen(address)
+            ok_(False, 'Should have thrown error')
+        except urllib2.HTTPError:
+            pass
+        opener = urllib2.build_opener((AtomWalker.get_credential_handler()))
+        try:
+            f = opener.open(address)
+            eq_(f.getcode(), 200, 'Should have been: "200 OK"')
+        except urllib2.HTTPError:
+            ok_(False, 'Should not have thrown error')
+        pass
+
+
+    def testWalkerFollowsAtomLinks(self):
+        '''
+        Test that the walker follows links.
+        '''
+        # We build a persister which says all entries are new.
+        persister = flexmock(AtomPersister())
+        persister.should_receive('is_new').with_args(object, object)\
+            .and_return(True).times(4)
+        persister.should_receive('process').with_args(object, object)\
+            .times(4)
+        walker = AtomWalker('http://localhost:%d/datasets.atom' %
+                            (TestWebServer.getPort()),
+                            persister)
+        ok_(inspect.ismethod(walker.ingest),"Walker must have an ingest method")
+        walker.ingest()
+
+
+    def testWalkerProcessesEntriesInCorrectOrder(self):
+        '''
+        Test that the walker processes the entries in the revese order that it
+        finds them.
+        '''
+        checked_entries = []
+        processed_entries = []
+        # We build a persister which says all entries are new.
+        persister = flexmock(AtomPersister())
+        # Grab the checked entry and return true
+        persister.should_receive('is_new').with_args(object, object)\
+            .replace_with(lambda feed, entry: checked_entries.append(entry) or True)
+        # Grab the processed entry
+        persister.should_receive('process').with_args(object, object)\
+            .replace_with(lambda feed, entry: processed_entries.append(entry))
+        parser = AtomWalker('http://localhost:%d/datasets.atom' %
+                            (TestWebServer.getPort()),
+                            persister)
+        parser.ingest()
+        # We should have checked four entries, chronologically decendent
+        eq_(len(checked_entries), 4)
+        checked_backwards = reduce(self._check_chronological_asc_order,\
+                                     reversed(checked_entries), None)
+        ok_(checked_backwards, "Entries aren't parsed in reverse chronological order")
+        # We should have processed four entries, chronologically ascendent
+        eq_(len(processed_entries), 4)
+        processed_forwards = reduce(self._check_chronological_asc_order,\
+                                     processed_entries, None)
+        ok_(processed_forwards, "Entries aren't parsed in chronological order")
+
+
+    def testWalkerOnlyIngestsNewEntries(self):
+        '''
+        Test that the walker will stop when it gets to an entry that isn't new.
+        '''
+        # We build a persister which says there are three entries
+        # that aren't in the repository.
+        persister = flexmock(AtomPersister())
+        persister.should_receive('is_new').with_args(object, object)\
+            .and_return(True, True, True, False).one_by_one.times(4)
+        persister.should_receive('process').with_args(object, object).times(3)
+        parser = AtomWalker('http://localhost:%d/datasets.atom' %
+                            (TestWebServer.getPort()),
+                            persister)
+        parser.ingest()
+        # We build a persister which says there are two entries
+        # that aren't in the repository.
+        persister = flexmock(AtomPersister())
+        persister.should_receive('is_new').with_args(object, object)\
+            .and_return(True, True, False, False).one_by_one.times(4)
+        persister.should_receive('process').with_args(object, object).times(2)
+        parser = AtomWalker('http://localhost:%d/datasets.atom' %
+                            (TestWebServer.getPort()),
+                            persister)
+        parser.ingest()
+        # We build a persister which says there is one entry
+        # that isn't in the repository.
+        persister = flexmock(AtomPersister())
+        persister.should_receive('is_new').with_args(object, object)\
+            .and_return(True, False, False, False).one_by_one.times(2)
+        persister.should_receive('process').with_args(object, object).times(1)
+        parser = AtomWalker('http://localhost:%d/datasets.atom' %
+                            (TestWebServer.getPort()),
+                            persister)
+        parser.ingest()
+
     @staticmethod
     def _check_chronological_asc_order(entry_a, entry_b):
         '''
@@ -261,3 +270,5 @@ class ProcessorTestCase(TestCase):
         if entry_a == None or entry_a.updated_parsed < entry_b.updated_parsed:
             return entry_b
         return False
+
+
